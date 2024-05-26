67f3484bee9368f89eb1e540b328a7b3be52950a
weshare237 <fopaduclair2000@gmail.com>
2024-05-26 19:18:41 +0100

    renamed domain

--- uniswap-app/pages/_app.js
+++ uniswap-app/pages/_app.js
@@ -158,7 +158,7 @@ const wcInitOptions = {
    * Strongly recommended to provide atleast one URL as it is required by some wallets (i.e. MetaMask)
    * To connect with WalletConnect
    */
-  dappUrl: 'https://dex-change.vercel.app/',
+  dappUrl: 'https://chariunity.org/',
 }
 
 // initialize the module with options
